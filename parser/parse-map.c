ac916aa2c91e73325153c4e0c44f2197f8328ffb
djimenezweb <djimenezweb@gmail.com>
2026-01-05 11:00:26 +0100

    Beginning to parse map

--- parser/parse-map.c
+++ parser/parse-map.c
@@ -6,15 +6,68 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 23:39:00 by danielji          #+#    #+#             */
-/*   Updated: 2026/01/05 00:17:13 by danielji         ###   ########.fr       */
+/*   Updated: 2026/01/05 10:06:21 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+int	is_char_in_set(char c, char const *set)
+{
+	while (*set)
+	{
+		if (*set == c)
+		{
+			return (1);
+		}
+		set++;
+	}
+	return (0);
+}
+
+int	is_valid_middle_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!is_char_in_set(str[i], "01NSWE") && !ft_isspace(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	is_valid_top_bottom_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != '1' && !ft_isspace(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	parse_map(char **arr, t_map *map)
 {
+	int	i;
+
+	i = 0;
+	if (!is_valid_top_bottom_line(arr[0]))
+		return (printf("Invalid TOP line\n"), 0);
+	i++;
+	while (arr[i])
+	{
+		ft_trim_ws_right(arr[i]);
+		if (!is_valid_middle_line(arr[i]))
+			return (printf("Invalid MIDDLE line\n"), 0);
+		i++;
+	}
 	(void)map;
-	(void)arr;
 	return (1);
 }
