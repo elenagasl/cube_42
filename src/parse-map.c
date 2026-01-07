10b9e3845174f0b6db77344fb45ab0ebad9b6b14
Daniel Jiménez <danielji@student.42madrid.com>
2026-01-07 11:09:27 +0100

    Validate '0' characters

--- src/parse-map.c
+++ src/parse-map.c
@@ -6,16 +6,14 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 23:39:00 by danielji          #+#    #+#             */
-/*   Updated: 2026/01/06 22:59:03 by danielji         ###   ########.fr       */
+/*   Updated: 2026/01/07 10:18:28 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
 /*
-TODO: Normalizar mapa (todas las líneas de la misma longitud)
 TODO: Medir dimensiones 
-TODO: Validar posición player
 TODO: Obtener coordenadas player */
 void	parse_map(t_game *g, char **arr)
 {
@@ -26,12 +24,15 @@ void	parse_map(t_game *g, char **arr)
 	while (!is_first_map_line(arr[i]))
 		i++;
 	player = get_player(&arr[i]);
-	if (player && is_valid_map(&arr[i], player))
-		printf("\033[1;32mMap is valid!\033[0m\n");
+	if (!player)
+		return ;
+	if (!is_valid_map(&arr[i], player))
+		return ;
+	if (!flood_fill(&arr[i]))
+		return ;
+	printf("\033[1;32mMap is valid!\033[0m\n");
+	print_parsed_data(g);
 	(void)g;
-/*
-	if (!flood_fill(arr, start, end))
-		return (printf("Error: Space not surrounded by wall\n"), 0); */
 }
 
 int	is_valid_map(char **arr, char player)
@@ -75,8 +76,8 @@ int	is_valid_top_bottom_line(char *str)
 
 int	is_valid_map_line(char *str, char p)
 {
-	int		i;
-	int		len;
+	int	i;
+	int	len;
 
 	len = ft_strlen(str) - 1;
 	while (ft_isspace(str[len]))
@@ -134,35 +135,19 @@ int	is_char_in_set(char c, char const *set)
 	return (0);
 }
 
-/* 
-// A '0' can't be surrounded by space OR NEW LINE, OR '\0', OR TAB, etc
-// TODO: ¡Cuidado! Al hacer trim los espacios han desaparecido
+/* A '0' can't be surrounded by space */
 int	is_surrounded(char **arr, int x, int y)
-{
-	if (arr[x - 1][y - 1] == ' '
-		|| arr[x][y - 1] == ' '
-		|| arr[x + 1][y - 1] == ' '
-		|| arr[x - 1][y] == ' '
-		|| arr[x + 1][y] == ' '
-		|| arr[x - 1][y + 1] == ' '
-		|| arr[x][y + 1] == ' '
-		|| arr[x + 1][y + 1] == ' ')
-		return (0);
-	return (1);
-}
-
-int	flood_fill(char **arr, int start, int end)
 {
 	int	i;
 	int	j;
 
-	i = start + 1;
-	while (i < end)
+	i = -1;
+	while(i < 2)
 	{
-		j = 0;
-		while (arr[i][j])
+		j = -1;
+		while (j < 2)
 		{
-			if (arr[i][j] == '0' && !is_surrounded(arr, i, j))
+			if (ft_isspace(arr[x + i][y + j]))
 				return (0);
 			j++;
 		}
@@ -170,4 +155,23 @@ int	flood_fill(char **arr, int start, int end)
 	}
 	return (1);
 }
- */
+
+int	flood_fill(char **arr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (arr[i])
+	{
+		j = 0;
+		while (arr[i][j])
+		{
+			if (arr[i][j] == '0' && !is_surrounded(arr, i, j))
+				return (printf("Error: Space not surrounded by wall\n"), 0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
