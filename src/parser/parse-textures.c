21ac6bf34ae941f7c54f33952b92e1f29c13623a
djimenezweb <djimenezweb@gmail.com>
2026-01-14 00:17:54 +0100

    Rearranged file

--- src/parser/parse-textures.c
+++ src/parser/parse-textures.c
@@ -6,28 +6,45 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/04 18:05:32 by danielji          #+#    #+#             */
-/*   Updated: 2026/01/13 22:48:21 by danielji         ###   ########.fr       */
+/*   Updated: 2026/01/13 23:53:48 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
-/* Iterate through cub file and parse paths to textures */
-void	parse_textures(t_game *g, char **arr)
+/* Check if line is a texture line */
+int	is_texture(char *str)
 {
 	int	i;
 
+	if (!str)
+		return (0);
 	i = 0;
-	while (arr[i])
-	{
-		if (is_texture(arr[i]))
-			get_texture_path(g->text_paths, arr[i]);
+	while (ft_isspace(str[i]))
 		i++;
-	}
+	if (ft_strncmp(&str[i], "NO", 2) == 0
+		|| ft_strncmp(&str[i], "SO", 2) == 0
+		|| ft_strncmp(&str[i], "WE", 2) == 0
+		|| ft_strncmp(&str[i], "EA", 2) == 0)
+		return (str[i + 2] && ft_isspace(str[i + 2]));
+	return (0);
+}
+
+static int	cardinal_to_index(char c)
+{
+	if (c == 'N')
+		return (NO);
+	else if (c == 'S')
+		return (SO);
+	else if (c == 'W')
+		return (WE);
+	else if (c == 'E')
+		return (EA);
+	return (-1);
 }
 
 /* Save a valid path to a texture file into array of textures */
-void	get_texture_path(char *textures[4], char *line)
+static void	get_texture_path(char *textures[4], char *line)
 {
 	int		i;
 	int		fd;
@@ -56,48 +73,16 @@ void	get_texture_path(char *textures[4], char *line)
 	return ;
 }
 
-/* Check if line is a texture line */
-int	is_texture(char *str)
+/* Iterate through cub file and parse paths to textures */
+void	parse_textures(t_game *g, char **arr)
 {
 	int	i;
 
-	if (!str)
-		return (0);
 	i = 0;
-	while (ft_isspace(str[i]))
+	while (arr[i])
+	{
+		if (is_texture(arr[i]))
+			get_texture_path(g->text_paths, arr[i]);
 		i++;
-	if (ft_strncmp(&str[i], "NO", 2) == 0
-		|| ft_strncmp(&str[i], "SO", 2) == 0
-		|| ft_strncmp(&str[i], "WE", 2) == 0
-		|| ft_strncmp(&str[i], "EA", 2) == 0)
-		return (str[i + 2] && ft_isspace(str[i + 2]));
-	return (0);
-}
-
-// TODO Mejor usar enum?
-int	cardinal_to_index(char c)
-{
-	if (c == 'N')
-		return (NO);
-	else if (c == 'S')
-		return (SO);
-	else if (c == 'W')
-		return (WE);
-	else if (c == 'E')
-		return (EA);
-	return (-1);
-}
-
-// TODO Mejor usar enum?
-int	index_to_cardinal(int i)
-{
-	if (i == NO)
-		return ('N');
-	else if (i == SO)
-		return ('S');
-	else if (i == WE)
-		return ('W');
-	else if (i == EA)
-		return ('E');
-	return ('0');
+	}
 }
