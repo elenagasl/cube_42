93855e00c2a575e9c565cc85322c360e68ae97da
Daniel Jiménez <danielji@student.42madrid.com>
2026-01-16 11:28:41 +0100

    Alternative movement files

--- src/game/movement_alt2.c
+++ src/game/movement_alt2.c
@@ -1,44 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   movement_alt.c                                     :+:      :+:    :+:   */
+/*   movement_alt2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 16:23:27 by elengarc          #+#    #+#             */
-/*   Updated: 2026/01/16 11:26:19 by danielji         ###   ########.fr       */
+/*   Updated: 2026/01/16 11:24:17 by danielji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
+#define MARG 0.5
+
 int	is_walkable(t_game *g, int x, int y)
 {
-	if (x < 1 || x >= g->map_w || y < 1 || y >= g->map_h)
+	if (x < 0 || x >= g->map_w || y < 0 || y >= g->map_h)
 		return (0);
 	return (g->map[y][x] == 0);
-}
-
-int	is_pos_x_walkable(t_game *g, double new_x)
-{
-	double	margin;
-
-	margin = 0.1;
-	if (is_walkable(g, (int)(new_x + margin), (int)g->player.pos_y)
-		&& is_walkable(g, (int)(new_x - margin), (int)g->player.pos_y))
-			return (1);
-	return (0);
-}
-
-int	is_pos_y_walkable(t_game *g, double new_y)
-{
-	double	margin;
-
-	margin = 0.1;
-	if (is_walkable(g, (int)g->player.pos_x, (int)(new_y + margin))
-		&& is_walkable(g, (int)g->player.pos_x, (int)(new_y - margin)))
-			return (1);
-	return (0);
 }
 
 void	rotate(t_player *p, double rot_speed)
@@ -54,30 +34,48 @@ void	rotate(t_player *p, double rot_speed)
 	p->plane_y = old_plane_x * sin(rot_speed) + p->plane_y * cos(rot_speed);
 }
 
-void	move_lateral(t_game *g, double speed)
+void	move_longitudinal(t_game *g, double speed, int dir)
 {
 	double	new_x;
 	double	new_y;
+	double	marg_x;
+	double	marg_y;
 
-	new_x = g->player.pos_x + g->player.plane_x * speed;
-	new_y = g->player.pos_y + g->player.plane_y * speed;
-	if (is_pos_x_walkable(g, new_x))
+	new_x = g->player.pos_x + (dir * g->player.dir_x * speed);
+	new_y = g->player.pos_y + (dir * g->player.dir_y * speed);
+	marg_x = dir * g->player.dir_x * MARG;
+	marg_y = dir * g->player.dir_y * MARG;
+	if (is_walkable(g, (int)(new_x + marg_x), (int)(g->player.pos_y)))
 		g->player.pos_x = new_x;
-	if (is_pos_y_walkable(g, new_y))
+	if (is_walkable(g, (int)(g->player.pos_x), (int)(new_y + marg_y)))
 		g->player.pos_y = new_y;
+/* 	if (is_walkable(g, (int)(new_x + marg_x), (int)(new_y + marg_y)))
+	{
+		g->player.pos_x = new_x;
+		g->player.pos_y = new_y;
+	} */
 }
 
-void	move_longitudinal(t_game *g, double speed)
+void	move_lateral(t_game *g, double speed, int dir)
 {
 	double	new_x;
 	double	new_y;
+	double	marg_x;
+	double	marg_y;
 
-	new_x = g->player.pos_x + g->player.dir_x * speed;
-	new_y = g->player.pos_y + g->player.dir_y * speed;
-	if (is_pos_x_walkable(g, new_x))
+	new_x = g->player.pos_x + (dir * g->player.plane_x * speed);
+	new_y = g->player.pos_y + (dir * g->player.plane_y * speed);
+	marg_x = dir * g->player.plane_x * MARG;
+	marg_y = dir * g->player.plane_y * MARG;
+	if (is_walkable(g, (int)(new_x + marg_x), (int)(g->player.pos_y)))
 		g->player.pos_x = new_x;
-	if (is_pos_y_walkable(g, new_y))
+	if (is_walkable(g, (int)(g->player.pos_x), (int)(new_y + marg_y)))
 		g->player.pos_y = new_y;
+/* 	if (is_walkable(g, (int)(new_x + marg_x), (int)(new_y + marg_y)))
+	{
+		g->player.pos_x = new_x;
+		g->player.pos_y = new_y;
+	} */
 }
 
 void	update_player(t_game *g)
@@ -88,13 +86,13 @@ void	update_player(t_game *g)
 	move_speed = g->frame_time * 5.0;
 	rot_speed = g->frame_time * 3.0;
 	if (g->key_w)
-		move_longitudinal(g, move_speed);
+		move_longitudinal(g, move_speed, 1);
 	if (g->key_s)
-		move_longitudinal(g, -move_speed);
+		move_longitudinal(g, move_speed, -1);
 	if (g->key_a)
-		move_lateral(g, -move_speed);
+		move_lateral(g, move_speed, -1);
 	if (g->key_d)
-		move_lateral(g, move_speed);
+		move_lateral(g, move_speed, 1);
 	if (g->key_left)
 		rotate(&g->player, -rot_speed);
 	if (g->key_right)
